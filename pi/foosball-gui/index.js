07cbd36997f5cc770c212024e0e503dde047abe4
slayterteal <slayterteal@gmail.com>
2022-03-24 19:36:39 -0500

    gui now polls server correctly

--- pi/foosball-gui/index.js
+++ pi/foosball-gui/index.js
@@ -1,16 +1,18 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const AutoLaunch = require('auto-launch');
 const path = require("path");
 
+// create and display window ===========================================
+// load the respective html
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 600,
+    fullscreen: true,
     webPreferences: {
-      preload: path.join(__dirname, "./lib/preload.js"),
+      preload: path.join(__dirname, "./src/preload.js"),
     },
   });
   // mainWindow.setMenu(null);
-  mainWindow.loadFile("./lib/index.html");
+  mainWindow.loadFile("./src/index.html");
 };
 
 app.whenReady().then(() => {
@@ -18,12 +20,63 @@ app.whenReady().then(() => {
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    // Set autolaunch ==================================================
+    let autoLauncher = new AutoLaunch({
+      name: "Foosbots-GUI"
+    });
+
+    autoLauncher.isEnabled().then((isEnabled) => {
+      if(isEnabled) return;
+      console.log("Autolauncher enabled :)");
+      autoLauncher.enable();
+    }).catch((err) => {
+      throw err;
+    });
+    // -----------------------------------------------------------------
   });
 });
+
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
+// ---------------------------------------------------------------------
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// connecting to the server ============================================
+//const net = require("net");
+
+//const HOST = "192.168.0.1";
+//const PORT = 5000;
+
+//let serverRequest = {
+  //"action": "DUMP"
+//};
+
+//window.addEventListener("load", () => {
+  //document.getElementById("startGameBtn").addEventListener("click", () => {
+    //console.log("start game button");
+  //});
+  //document.getElementById("endGameBtn").addEventListener("click", () => {
+    //console.log("end game button");
+  //});
+//});
+
+//window.setTimeout(() => {console.log("Hello...")}, 50);
+
+//const client = new net.Socket();
+//client.connect({port: PORT, host: HOST});
+//setTimeout(() => {
+  //console.log(JSON.stringify(serverRequest));
+  //if (client.write("empt"+JSON.stringify(serverRequest))){
+    //client.on('data', (data) => {
+      //// console.log(data.toString('utf-8'));
+      //// update elements of the DOM here.
+      
+    //});
+  //}
+  //else {
+    //console.log("Did not send all data...");
+  //}
+//}, 50);
+
+
--- pi/foosball-gui/src/preload.js
+++ pi/foosball-gui/src/preload.js
@@ -1,14 +1,35 @@
 // preload.js
+const net = require("net");
+
+const HOST = "192.168.0.1";
+const PORT = 5000;
+
+let serverRequest = {
+  "action": "DUMP"
+};
 
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
-
-  for (const dependency of ["chrome", "node", "electron"]) {
-    replaceText(`${dependency}-version`, process.versions[dependency]);
-  }
+window.addEventListener("load", () => {
+  document.getElementById("startGameBtn").addEventListener("click", () => {
+    console.log("start game button");
+  });
+  document.getElementById("endGameBtn").addEventListener("click", () => {
+    console.log("end game button");
+  });
 });
+
+const client = new net.Socket();
+client.connect({port: PORT, host: HOST});
+setInterval(() => {
+  console.log(JSON.stringify(serverRequest));
+  if (client.write("empt"+JSON.stringify(serverRequest))){
+    client.on('data', (data) => {
+      console.log(data.toString('utf-8'));
+      // update elements of the DOM here.
+    });
+  }
+  else {
+    console.log("Did not send all data...");
+  }
+}, 500);
